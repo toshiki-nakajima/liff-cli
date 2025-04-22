e9134cad37f06d78098f25b22f3ac8ee533147c4
toshiki-nakajima <72725615+toshiki-nakajima@users.noreply.github.com>
2025-04-22 19:21:12 +0900

    liff_uid

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,36 @@ import type { Liff } from "@line/liff";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
   liff,
   liffError
 }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    if (!liff) {
+      console.log("LIFF is not initialized.");
+      return;
+    }
+    console.log("LIFF init succeeded.");
+    console.log(liff);
+    console.log(liff.getDecodedIDToken());
+    if (liff.isLoggedIn()) {
+      console.log("LIFF is logged in.");
+      setIsLoggedIn(true);
+    } else {
+      liff.login();
+      setIsLoggedIn(true);
+    }
+  }, [liff, isLoggedIn]);
+
+  const handleLogout = () => {
+    if (liff) {
+      liff.logout();
+      setIsLoggedIn(false);
+    }
+  };
   return (
     <div>
       <Head>
@@ -33,6 +58,11 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
         >
           LIFF Documentation
         </a>
+        {isLoggedIn
+          ? <div>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          : null}
       </main>
     </div>
   );
